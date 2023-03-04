086533f55532232a7b9b2656fa78d536259befda
Turetchi Gabriel <g.turetchi2001@gmail.com>
2023-03-04 18:39:29 +0200

    First Commit Bledi Tudor

---
+++ src/components_BABI_VERSION/UI.js
@@ -0,0 +1,12 @@
+import {Stack, Text} from "@chakra-ui/react";
+
+const UI = () => {
+    return (
+        <Stack>
+            <Text>Hello Test Page</Text>
+
+        </Stack>
+    );
+};
+
+export default UI;
---
+++ src/components_BABI_VERSION/test.js
@@ -0,0 +1,12 @@
+import {Stack, Text} from "@chakra-ui/react";
+
+const Test = () => {
+    return (
+        <Stack>
+            <Text>Hello Test Page</Text>
+            
+        </Stack>
+    );
+};
+
+export default Test;
